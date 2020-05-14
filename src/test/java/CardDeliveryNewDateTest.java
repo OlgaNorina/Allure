f17a62aea3c76d7a262f6d44aa106fc20b0fdeeb
Olga <norinaoa@mail.ru>
2020-05-14 23:00:05 +0500

    test was fixed

--- src/test/java/CardDeliveryNewDateTest.java
+++ src/test/java/CardDeliveryNewDateTest.java
@@ -1,40 +1,41 @@
 import com.codeborne.selenide.SelenideElement;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.util.Locale;
+import java.time.format.DateTimeFormatter;
+
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryNewDateTest {
-    private Faker faker;
-    private
-
+    private Meeting meeting = DataGenerator.Registration.generate();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    
     @BeforeEach
-    void setUpAll() {
-        faker = new Faker(new Locale("ru"));
+    void openURL() {
+        open("http://localhost:9999");
     }
 
     @Test
     void shouldSubmitRequest() {
 
-        open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue(faker.address().city());
-        $$(".menu-item").first().click();
-        form.$(".input__icon").click();
-        $$("td").find(exactText("21")).click();
-        form.$("[data-test-id=name] input").setValue(faker.name().lastName()).setValue(" ").setValue(faker.name().firstName());
-        form.$("[data-test-id=phone] input").setValue("+7").setValue(faker.phoneNumber().cellPhone());
+        form.$("[data-test-id=city] input").setValue(meeting.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(formatter.format(meeting.getDateFirstMeeting()));
+        form.$("[data-test-id=name] input").setValue(
+                meeting.getFirstName() + " " + meeting.getLastName());
+        form.$("[data-test-id=phone] input").setValue("+7" + meeting.getPhoneNumber());
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
 
-        form.$(".input__icon").click();
-        $$("td").find(exactText("23")).click();
+        form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(formatter.format(meeting.getDateSecondMeeting()));
         form.$(".button").click();
         $$(".button").find(exactText("Перепланировать")).click();
         $(".notification_status_ok").shouldBe(exist);
     }
 }
+
